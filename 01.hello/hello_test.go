e3ba0f67ae2a05f1ea37c28ef6f08416d13f293b
Aidar Khabirov <khabirov.ar1990@gmail.com>
2025-03-17 15:01:43 +0500

    Solve requirement

--- 01.hello/hello_test.go
+++ 01.hello/hello_test.go
@@ -4,12 +4,12 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "English")
 		want := "Hello, Chris!"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World!' when empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World!"
 		assertCorrectMessage(t, got, want)
 	})
