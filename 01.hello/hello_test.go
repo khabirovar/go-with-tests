28eef3043c419b541fd707f7df807a4c7bce5e0e
Aidar Khabirov <khabirov.ar1990@gmail.com>
2025-03-17 15:26:49 +0500

    Fix test default value for language

--- 01.hello/hello_test.go
+++ 01.hello/hello_test.go
@@ -4,12 +4,12 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris", "English")
+		got := Hello("Chris", "")
 		want := "Hello, Chris!"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World!' when empty string is supplied", func(t *testing.T) {
-		got := Hello("", "English")
+		got := Hello("", "")
 		want := "Hello, World!"
 		assertCorrectMessage(t, got, want)
 	})
