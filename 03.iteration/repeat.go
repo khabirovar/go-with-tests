259d2a94665f05eec05c821c2b20b857d40e1ef5
Aidar Khabirov <khabirov.ar1990@gmail.com>
2025-04-09 23:17:42 +0500

    Refactor code

--- 03.iteration/repeat.go
+++ 03.iteration/repeat.go
@@ -1,10 +1,13 @@
 package iteration
 
+const repeatedCount = 5
+
 func Repeat(char string) string {
 	var repeated string
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatedCount; i++ {
 		repeated += char
 	}
+
 	return repeated
 }
