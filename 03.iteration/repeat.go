beca4baf3d37e180336bc8f4ec070c921823cbbe
Aidar Khabirov <khabirov.ar1990@gmail.com>
2025-04-21 22:17:32 +0500

    Move repeat count in params

--- 03.iteration/repeat.go
+++ 03.iteration/repeat.go
@@ -4,10 +4,10 @@ import "strings"
 
 const repeatedCount = 5
 
-func Repeat(char string) string {
+func Repeat(char string, count int ) string {
 	var repeated strings.Builder
 
-	for i := 0; i < repeatedCount; i++ {
+	for i := 0; i < count; i++ {
 		repeated.WriteString(char)
 	}
 
